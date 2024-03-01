98836b4f89d230f736e76420d72570a171cf4b7b
akhand08 <adib08@outlook.com>
2024-03-01 17:26:19 -0300

    login router added

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-const passport = require("passport")
 
 const userModel = require("./users");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+passport.authenticate(new localStrategy(userModel.authenticate()));
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+// register 
 
 router.post("/register", function(req, res) {
   const {username, password, email, fullname} = req.body;
@@ -16,12 +22,43 @@ router.post("/register", function(req, res) {
 
   userModel.register(newUser, password)
   .then(function() {
-    passport.authenticate("local")(req, res, function {
+    passport.authenticate("local")(req, res, function() {
       res.redirect("/profile");
     })
   })
- 
+  
 })
+
+
+// router.post("/register", (req, res) {
+//   const {username, password, fullname, email} = req.body;
+//   let newUser = new userModel.create({username, fullname, email});
+
+//   userModel.register(newUser, password)
+//   .then(function() {
+//     passport.authenticate("local")(req, res, function() {
+//       res.redirect("profile");
+//     })
+//   })
+// })
+
+// login
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}), function(req, res) {
+
+})
+
+
+
+// router.post("/login", passport.authenticate("local", {
+//   successRedirect: "/profile",
+//   failureRedirect: "/"
+// }), functon(req, res) {})
+
+
 
 
 
