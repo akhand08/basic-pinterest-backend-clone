daaea3e4fc1ef0a6acb6a21c6503120de6308dd8
akhand08 <adib08@outlook.com>
2024-03-01 11:18:07 -0300

    Register router has been created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+const passport = require("passport")
+
+const userModel = require("./users");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post("/register", function(req, res) {
+  const {username, password, email, fullname} = req.body;
+  let newUser = new userModel.create({username, email, fullname});
+
+  userModel.register(newUser, password)
+  .then(function() {
+    passport.authenticate("local")(req, res, function {
+      res.redirect("/profile");
+    })
+  })
+ 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
