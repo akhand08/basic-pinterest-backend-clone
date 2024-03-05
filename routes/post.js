422ce637fc9ab87d5f9028003062f981797f4ce4
akhand08 <adib08@outlook.com>
2024-03-05 09:49:28 -0300

    Post Model has been created

---
+++ routes/post.js
@@ -0,0 +1,30 @@
+
+
+const mongoose = require("mongoose")
+// const plm = require("passport-local-mongoose");
+
+
+// mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
+
+
+const postSchema = new mongoose.Schema({
+    postText: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    users: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    },
+    likes: {
+        type: Array,
+        default: []
+    }
+
+})
+
+module.exports = mongoose.model("posts", postSchema);
